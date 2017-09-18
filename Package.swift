850c043b11e550298f4e2e804402862ce224c64f
Klaas <klaas@users.noreply.github.com>
2017-09-18 10:21:07 +0200

    Swift 4

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "StringScanner"
+	name: "StringScanner",
+	products: [
+		.library( name: "StringScanner", targets: ["StringScanner"]),
+    ],
+    dependencies:[
+	],
+	targets: [
+		.target(name: "StringScanner", dependencies: [], path: "Sources"),
+	]
 )
+
+
